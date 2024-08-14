f5ca26342fd1b06881fb1423624463c7827a7b92
AkShAy21211 <akshay68580@gmail.com>
2024-08-14 20:39:22 +0530

    create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import { SignIn, SignUp } from '@clerk/nextjs'
+import Profile from './pages/Profile'
 const App = () => {
   return (
-    <div className='text-red-500'>
-      APP
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </Router> 
   )
 }
 
